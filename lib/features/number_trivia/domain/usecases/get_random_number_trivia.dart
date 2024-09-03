57f026757c98599e9a85b42233961033c9bf93b1
Jadiel Bett <jadielbett@gmail.com>
2024-09-03 15:33:34 +0300

    Add getRandomNumberTrivia usecase

---
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository.dart';
+
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
+  final NumberTriviaRepository numberTriviaRepository;
+
+  GetRandomNumberTrivia({required this.numberTriviaRepository});
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) {
+    return numberTriviaRepository.getRandomNumberTrivia();
+  }
+}
