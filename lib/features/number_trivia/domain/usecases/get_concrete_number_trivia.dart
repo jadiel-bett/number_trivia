c3ff1a964800b77d2f70080d135ed87aa5636a73
Jadiel Bett <jadielbett@gmail.com>
2024-09-03 13:25:06 +0300

    Add getConcreteNumberTrivia useCase

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/core/error/failures.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository numberTriviaRepository;
+
+  GetConcreteNumberTrivia({required this.numberTriviaRepository});
+
+  Future<Either<Failure, NumberTrivia>> execute({
+    required int number,
+  }) async {
+    return await numberTriviaRepository.getConcreteNumberTrivia(number);
+  }
+}
