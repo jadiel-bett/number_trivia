a0e4b80d03c01589337b47bee38a50445d0473ec
Jadiel Bett <jadielbett@gmail.com>
2024-09-16 13:04:08 +0300

    Implement NumberTriviaRepository

---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/core/platform/network_info.dart';
+import 'package:number_trivia/features/number_trivia/data/sources/number_trivia_local_data_source.dart';
+import 'package:number_trivia/features/number_trivia/data/sources/number_trivia_remote_data_source.dart';
+import '../../../../core/error/failures.dart';
+import '../../domain/entities/number_trivia.dart';
+import '../../domain/repositories/number_trivia_repository.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NetworkInfo networkInfo;
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+
+  NumberTriviaRepositoryImpl({
+    required this.networkInfo,
+    required this.remoteDataSource,
+    required this.localDataSource,
+  });
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
+    // TODO: implement getConcreteNumberTrivia
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
+    // TODO: implement getRandomNumberTrivia
+    throw UnimplementedError();
+  }
+}
