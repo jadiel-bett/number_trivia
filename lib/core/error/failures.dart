cf4912727eb8c1bf4241a7003ca78c110ffeddd3
Jadiel Bett <jadielbett@gmail.com>
2024-09-16 15:33:44 +0300

    Refactor code

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -6,8 +6,7 @@ abstract class Failure extends Equatable {
 
 class ServerFailure extends Failure {
   @override
-  // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [];
 }
 
 class CacheFailure extends Failure {
